3627b5b600046c3eb69bfbe6e6cc94946a96b7f4
PC <povilast@hotmail.lt>
2021-01-10 00:38:34 +0200

    add empty input alert

--- page.py
+++ page.py
@@ -1,21 +1,12 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, flash
 import prover_output as prover_main
 
 app = Flask(__name__)
+app.secret_key = 'supersecret'
 
-
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
-
-
-@app.route("/sequent", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        sequent_input = request.form["sequent-input"]
-        return redirect(url_for("proof", seq=sequent_input))
-    else:
-        return render_template("proof.html")
 
 
 @app.route("/proof/<seq>")
@@ -28,6 +19,21 @@ def proof(seq):
     print(proof_list)
 
     return render_template("proof.html", sequent=seq, boolean_result=result, proof='\n'.join(map(str, proof_list)))
+
+
+# Message flashing
+@app.route("/sequent", methods=["POST", "GET"])
+def sequent():
+    if request.method == "POST":
+        req = request.form
+        sequent_input = request.form["sequent-input"]
+        if len(sequent_input) == 0:
+            flash('Įveskite sekvenciją!')
+            return redirect(url_for("home"))
+        else:
+            return redirect(url_for("proof", seq=sequent_input))
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
